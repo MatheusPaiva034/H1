782cef3660799e8bf1aef72df4923326f137ec5f
MPB <matheuspaivab2013@gmail.com>
2026-09-06 20:24:21 -0300

    Lista 2 – Desenvolvimento Web

---
+++ Exercicios H1 02/Exercicios H1 02/models/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercicios_H1_02.models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+
+        public double IMC
+        {
+            get
+            {
+                return Peso / (Altura * Altura);
+            }
+        }
+    }
+}
